14be7ce1989f1eb31e8f723fca8813db67500dde
Laynezilla <Laynezilla@users.noreply.github.com>
2018-12-30 16:12:55 -0500

    Create bootstart.sh

---
+++ bootstart.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Require clients to download workshop resources
+echo "resource.AddWorkshop("$WORKSHOP_COLLECTION")" > ./gmod/garrysmod/lua/autorun/server/workshop.lua;
+
+# Update all content
+./steamcmd/steamcmd.sh +login anonymous +force_install_dir ./css-dedicated +app_update 232330 +quit;
+
+# Start server
+./css-dedicated/srcds_run -game cstrike +maxplayers $MAX_PLAYERS +map $MAP +gamemode $GAMEMODE +host_workshop_collection $WORKSHOP_COLLECTION -authkey $AUTH_KEY;
